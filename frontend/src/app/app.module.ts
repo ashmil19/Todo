f9f3d039d74632801efc66da88f3f525cde9ce52
ashmil19 <ashmilabdulrasheed@gmail.com>
2022-06-08 21:30:24 +0530

    frontend todo-fakeApi get function created

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodolistComponent } from './components/todolist/todolist.component';
@@ -25,7 +27,7 @@ import { AutosizeModule } from '@techiediaries/ngx-textarea-autosize';
     MatDialogModule,
     MatButtonToggleModule,
     AutosizeModule,
-
+    HttpClientModule,
   ],
   providers: [ ApiService ],
   bootstrap: [AppComponent]
--- frontend/src/app/components/todolist/todolist.component.ts
+++ frontend/src/app/components/todolist/todolist.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewChild, TemplateRef, ElementRef } from '@angular/
 
 import { MatDialog } from '@angular/material/dialog';
 
+import { ApiService } from 'src/app/services/api.service';
+
 @Component({
   selector: 'app-todolist',
   templateUrl: './todolist.component.html',
@@ -15,9 +17,34 @@ export class TodolistComponent implements OnInit {
   dialogRef!: TemplateRef<any>;
 
 
-  constructor(public dialog:MatDialog) {}
+  constructor(public dialog:MatDialog, private api:ApiService) {}
 
   ngOnInit(): void {
+    this.getTodayData();
+    this.getWeekData();
+    this.getMonthData();
+  }
+
+  today:any;
+  week:any;
+  month:any;
+
+  getTodayData(){
+    this.api.getToday().subscribe(data =>{
+      this.today = data;
+    })
+  }
+
+  getWeekData(){
+    this.api.getWeek().subscribe(data =>{
+      this.week = data;
+    })
+  }
+
+  getMonthData(){
+    this.api.getMonth().subscribe(data =>{
+      this.month = data;
+    })
   }
 
   openDialog(){
