58c02c5727ddebc8c45e51c38bba403307cbc47b
ashmil19 <ashmilabdulrasheed@gmail.com>
2022-06-08 21:13:42 +0530

    frontend fake-api created

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor( private http:HttpClient ) { }
+
+  getToday(){
+    return this.http.get('http://localhost:3000/today/');
+  }
+
+  getWeek(){
+    return this.http.get('http://localhost:3000/week/');
+  }
+
+  getMonth(){
+    return this.http.get('http://localhost:3000/month/');
+  }
 }
