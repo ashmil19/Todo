d168901b07a7607201a596f9eb92b74a58cada73
ashmil19 <ashmilabdulrasheed@gmail.com>
2022-07-05 17:10:06 +0530

    backend view class added

--- backend/todo_api/views.py
+++ backend/todo_api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
+
+from .models import *
+from .serializers import *
+
 # Create your views here.
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = TodoListItem.objects.all()
+    serializer_class = TodoListItemSerializer
