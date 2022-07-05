19db0d3503b30429fa855b360a6b42ba8b2ae56b
ashmil19 <ashmilabdulrasheed@gmail.com>
2022-07-05 17:24:40 +0530

    backend urls added

--- backend/todo_api/urls.py
+++ backend/todo_api/urls.py
@@ -1,3 +1,11 @@
+from django.urls import path, include
+from .views import *
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+router.register('api', TodoViewSet, basename='todo')
+
 urlpatterns = [
-    
+    path('', include(router.urls)),
 ]
