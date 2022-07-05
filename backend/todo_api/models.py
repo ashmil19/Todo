7971bea10bb81aa4159636a81b7145974d2adf1b
ashmil19 <ashmilabdulrasheed@gmail.com>
2022-07-05 12:39:14 +0530

    backend models class added

--- backend/todo_api/models.py
+++ backend/todo_api/models.py
@@ -1,8 +1,11 @@
 from django.db import models
-
+from .choices import WhenChoice
 # Create your models here.
 
 class TodoListItem(models.Model):
     content = models.CharField(max_length=100, null=False, help_text='This field is required')
     isDone = models.BooleanField(default=False)
-    when = models.CharField(max_length=100)
+    when = models.CharField(max_length=100, choices=WhenChoice.choices, default=WhenChoice.TODAY)
+
+    def __str__(self):
+        return self.content
