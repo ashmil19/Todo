c91614a1df99b160007f8c9af7c0a078c9b4fc2b
ashmil19 <ashmilabdulrasheed@gmail.com>
2022-07-05 12:36:23 +0530

    backend choices class added

---
+++ backend/todo_api/choices.py
@@ -0,0 +1,6 @@
+from djchoices import DjangoChoices, ChoiceItem
+
+class WhenChoice(DjangoChoices):
+    TODAY = ChoiceItem('TD')
+    THIS_WEEK = ChoiceItem('TW')
+    THIS_MONTH = ChoiceItem('TM')
--- backend/todo_api/models.py
+++ backend/todo_api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class TodoListItem(models.Model):
+    content = models.CharField(max_length=100, null=False, help_text='This field is required')
+    isDone = models.BooleanField(default=False)
+    when = models.CharField(max_length=100)
